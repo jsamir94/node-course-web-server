abe9dd824cd3937a3399fd548ba35971bff61d24
jsamir94 <jsamir94@users.noreply.github.com>
2018-04-04 15:30:22 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+
+
+const express = require('express');
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+var now = new Date().toString();
+var log=`${now} : ${req.method} ${req.url}`;
+console.log(log);    
+fs.appendFile('server.log', log + '\n',(err)=>{
+    if(err){
+        console.log('Unable to append to server.log')
+    }
+});
+next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+// })
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+hbs.registerHelper('sreamIt',(text)=>{
+    return text.toUpperCase();
+})
+app.get('/',(req,res)=>{
+//res.send('<h1>Hello Express</h1>');
+res.render('home.hbs',{
+    pageTitle:'Home page',
+    welcomeMessage:'Welcome !! Have a nice day...',
+
+})
+});
+
+app.get('/about',(req,res)=>{
+   res.render('about.hbs',{
+       pageTitle:'About page',
+   })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send('Error!!!!!!!!!!!!!!')
+
+    
+})
+
+app.listen(3000,()=>{
+    console.log('server is up at 3000.')
+});
